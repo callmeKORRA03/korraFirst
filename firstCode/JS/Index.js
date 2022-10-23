c3406c438989afb4481621cbd0f4c975bca28322
callmeKORRA03 <116469228+callmeKORRA03@users.noreply.github.com>
2022-10-23 01:52:31 +0100

    Add files via upload

---
+++ firstCode/JS/Index.js
@@ -0,0 +1,44 @@
+//FOR DISPLAYIN DATE AND TIME
+function displayDate() {
+    var myDate = new Date();
+    var year = myDate.getYear();
+    if (year < 1000) {
+        year += 1900
+    }
+    var day = myDate.getDay();
+    var month = myDate.getMonth();
+    var daym = myDate.getDate();
+    var dayArray = new Array("Sunday", "Monday", "TuesDay", "WednesDay", "Thursday", "Friday", "Saturday");
+    var monthArray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+    var currentTime = new Date();
+    var h = currentTime.getHours();
+    var m = currentTime.getMinutes();
+    var s = currentTime.getSeconds();
+    if (h == 24) {
+        h = 0;
+    } else if (h > 12) {
+        h = h - 0;
+    }
+
+    if (h < 10) {
+        h = "0" + h;
+    }
+
+    if (m < 10) {
+        m = "0" + m;
+    }
+
+    if (s < 10) {
+        s = "0" + s;
+    }
+
+    var myCurrentDate = document.getElementById("Date"); myCurrentDate.textContent = " " +dayArray[day]+ ""  +daym+   ""   + monthArray[month]+  ""  +year +  " | " + h + ":" + m + ":" + s;
+    myCurrentDate.innerText = " "   +dayArray[day]+  ""  + daym +   ""   + monthArray[month]+  ""  +year+  " | " + h + ":" + m + ":" + s;
+
+    setTimeout("displayDate()", 1000);
+}
+displayDate();
+
+document.getElementById("Date").style.fontSize = "20px";
+document.getElementById("Date").style.textAlign = "center";
